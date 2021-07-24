ba98cb0bc0e5a7b55e6a27dba2a946804ee92a3e
ZAN <zanders.lsp@gmail.com>
2021-07-24 16:56:34 +0800

    first commit

---
+++ app/Models/Car.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CAr extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'brand',
+        'model',
+        'type',
+        'price',
+        'acquired_on',
+    ];
+
+    public function container() {
+        return $this->belongsTo('App\Models\Tool', 'acquired_on', 'id');
+    }
+}
